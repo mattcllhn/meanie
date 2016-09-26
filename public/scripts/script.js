9820d677363a9865285b01ccadbc22db59a55ee2
matt <mattcllhn@gmail.com>
2016-09-26 13:52:01 -0500

    cleanup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,32 @@
 var myApp=angular.module( 'myApp', [] );
 myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
-$scope.addRecord = function(){
-event.preventDefault();
-var objectToSend ={
-name: $scope.nameIn,
-location: $scope.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-$scope.nameIn ='';
-$scope.locationIn='';
-};
-$scope.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-$scope.allTheRecords = response;
-console.log( $scope.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
-}]);
+  $scope.addRecord = function(){
+    event.preventDefault();
+    var objectToSend ={
+      name: $scope.nameIn,
+      location: $scope.locationIn,
+    };//objectToSend
+    $http({
+      method: 'POST',
+      url: '/testPost',
+      data: objectToSend
+    }).then(function(response){
+    $scope.nameIn ='';
+    $scope.locationIn='';
+    console.log('hello from success function');
+
+  });//then function
+  };//addRecord function
+  $scope.getRecords = function(){
+    $http({
+      method: 'GET',
+      url: '/getRecords',
+    }).then( function( response ){
+      $scope.allTheRecords = response;
+      console.log( $scope.allTheRecords );
+      function myError( response ){
+      console.log( response.statusText );
+    }//error function
+    });//response error
+    };
+}]);//controller
--- server/app.js
+++ server/app.js
@@ -7,33 +7,40 @@ var mongoose = require( 'mongoose' );
 // 27017 is default mongo port
 mongoose.connect( 'localhost:/27017/test' );
 var ourSchema = new  mongoose.Schema({
-name: String
-location: String
+  name: String,
+  location: String
 });
 var ourModel = mongoose.model( 'ourModel', ourSchema );
 app.get( '/', function( req, res ){
-res.sendFile( path.resolve( 'public/index.html' ) );
+  res.sendFile( path.resolve( 'public/index.html' ) );
 });
 app.get( '/getRecords', function( req, res ){
-// get and send back all the things
-ourModel.find();
-.then( function( data ){
-res.send( data );
-});
+  // get and send back all the things
+  ourModel.find().then( function( data ){
+    res.send( data );
+  });
 });
 app.listen( 8080, 'localhost', function( req, res ){
-console.log( 'listening on 8080' );
+  console.log( 'listening on 8080' );
 });
 app.post( '/testPost', function( req, res ){
-console.log( 'req.body.name: ' + req.body.name );
-// retrieved the req.body
-// putting it into an object to be saved in the db
-var recordToAdd={
-name=req.body.name,
-location=req.body.location
-}
-// create new record
-var newRecord=ourModel( recordToAdd );
-newRecord.save();
+  console.log( 'req.body.name: ' + req.body.name+' '+req.body.location);
+  // retrieved the req.body
+  // putting it into an object to be saved in the db
+  var recordToAdd={
+    name:req.body.name,
+    location:req.body.location
+  };
+  // create new record
+  var newRecord=ourModel( recordToAdd );
+
+  newRecord.save(function(err){
+    if (err){
+      console.log(err);
+    }else{
+      console.log('hello from newrecord.save');
+      res.sendStatus(201);
+    }
+  });
 });
 app.use( express.static( 'public' ) );
